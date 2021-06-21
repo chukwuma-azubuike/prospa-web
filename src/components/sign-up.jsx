79add444953524fd26c7f3e3710b28c64616db95
soflarity <55975006+soflarity@users.noreply.github.com>
2021-06-21 09:49:46 +0100

    mobile responsive signup page

--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -4,17 +4,27 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
+import { useMediaQuery } from 'react-responsive';
 import SideIntroPanel from './side-intro-panel';
 import SignUpForm from './sign-up-form';
 
 function SignUp() {
+
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 540px)' });
+    const isBigScreen = useMediaQuery({ query: '(min-width: 541px)' })
+
     return (
         <div>
-            <SideIntroPanel />
-            <main style={{ height: '100%', background:'#FFFFFF' }} >
+            {isBigScreen && <div>
+                <SideIntroPanel />
+                <main style={{ height: '100%', background: '#FFFFFF' }} >
+                    <SignUpForm />
+                </main>
+            </div>}
+            {isTabletOrMobile && <div>
                 <SignUpForm />
-            </main>
+            </div>}
         </div>
     );
 }
